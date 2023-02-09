52ee59110d59341541dc5a571d480f29d19296a9
Jerick-CM <jmangaluz@gmail.com>
2023-02-09 14:21:13 +0800

    fix class name of migration mobile table

--- src/Providers/TestProvider.php
+++ src/Providers/TestProvider.php
@@ -37,12 +37,12 @@ class TestProvider extends ServiceProvider
 
             if (!class_exists('CreateMobilephonesTable')) {
                 $this->publishes([
-                    base_path() . '/vendor/jerickcm/yeastartg200/database/stub/create_mobilephones_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_create_mobilephones_table.php'),
+                    base_path() . '/vendor/jerickcm/yeastartg200/database/stub/create_mobilephones_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_mobilephones_table.php'),
                 ], 'migrations');
             }
 
             $this->publishes([
-                base_path() . '/vendor/jerickcm/yeastartg200/database/seeders/SimcardSeeder.php' => database_path('seeders/SimcardSeeder.php'),
+                base_path() . '/vendor/jerickcm/yeastartg200/database/seeder/SimcardSeeder.php' => database_path('seeders/SimcardSeeder.php'),
             ], 'seeds');
 
         }
